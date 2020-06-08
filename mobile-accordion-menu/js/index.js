e508567caa4e2cfd7c067d0b7c92c8384cf9943d
StepanKiryanenko <45694203+StepanKiryanenko@users.noreply.github.com>
2020-06-09 01:25:51 +0300

    add mobile accordion menu
    +

---
+++ mobile-accordion-menu/js/index.js
@@ -0,0 +1,18 @@
+$(function() {
+  function init() {
+    $('[data-behaviour="toggle-menu-icon"]').on('click', toggleMenuIcon);
+    $('[data-behaviour="toggle-link-icon"]').on('click', toggleSubMenu);
+  };
+  
+  function toggleMenuIcon() {
+    $(this).toggleClass('menu-icon--open');
+    $('[data-element="toggle-nav"]').toggleClass('nav--active');
+  };
+  
+  function toggleSubMenu() {
+    $(this).toggleClass('nav__link--plus nav__link--minus');
+    $('[data-behaviour="toggle-sub-menu"]').slideToggle('nav__sub-list--active');
+  };
+  
+  init()
+});
