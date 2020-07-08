04dc515bdf73dd49b97b51d3b66f07dee1a52b52
StepanKiryanenko <45694203+StepanKiryanenko@users.noreply.github.com>
2020-07-09 01:09:56 +0300

    add project PLANETO

---
+++ Planeto/js/js.custom.js
@@ -0,0 +1,37 @@
+	
+   // odometr
+    jQuery(function ($) {
+      // custom formatting example
+      $('#earth').data('countToOptions', {
+        formatter: function (value, options) {
+          return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+        }
+      });
+
+      // start all the timers
+      $('.timer').each(count);
+      function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+      }
+    });
+
+
+    // scroll menu
+
+    jQuery("document").ready(function($){
+ 
+	var nav = $('.navbar');
+ 
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 136) {
+			nav.addClass("f-nav");
+		} else {
+			nav.removeClass("f-nav");
+		}
+	});
+ 
+});
+
+ 
